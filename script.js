392ded23432e80cd945bff7ced9a291e953f23c8
Corbin Crutchley <git@crutchcorn.dev>
2022-12-24 05:19:24 -0800

    chore: initial work at supporting keyboard events

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ function rafAbortable(signal, cb) {
   requestAnimationFrame(() => rafAbortable(signal, cb));
 }
 
+function throttle(func, timeFrame) {
+  var lastTime = 0;
+  return function (...args) {
+    var now = Date.now();
+    if (now - lastTime >= timeFrame) {
+      func(...args);
+      lastTime = now;
+    }
+  };
+}
+
 /**
  * @param {number | string} number
  * @param {number} amountOfNumbers
@@ -30,59 +41,83 @@ function prefixNumberWithZeros(number, amountOfNumbers) {
   return strNum;
 }
 
-const el = document.querySelector('.bowtie-button');
-const bowties = el.querySelector('.bowties');
+const animDurationMs = 300;
+// Not including 0
+const frames = 15;
 
-el.addEventListener('pointerdown', () => {
+function startListening(cb) {
   const mouseDownController = new AbortController();
   const mouseDownSignal = mouseDownController.signal;
   const mouseDownCurrent = performance.now();
-  const animDurationMs = 300;
-  // Not including 0
-  const frames = 15;
-  let mouseDownCount = 0;
+  let mouseDownCount = { current: 0 };
   rafAbortable(mouseDownSignal, () => {
     const nowDiff = performance.now() - mouseDownCurrent;
     const progress = nowDiff / animDurationMs;
-    mouseDownCount = Math.floor(progress * frames);
-    if (mouseDownCount > frames) {
+    mouseDownCount.current = Math.floor(progress * frames);
+    if (mouseDownCount.current > frames) {
       mouseDownController.abort();
       return;
     }
     const frameNum = prefixNumberWithZeros(
-      mouseDownCount,
+      mouseDownCount.current,
       2
     );
     bowties.style.backgroundImage = `url("/bowtie-button-demo/assets/frame${frameNum}.svg")`;
   });
 
-  document.addEventListener(
-    'pointerup',
-    () => {
-      mouseDownController.abort();
-      const mouseUpController = new AbortController();
-      const mouseUpSignal = mouseUpController.signal;
-      const mouseUpCurrent = performance.now();
+  cb(mouseDownController, mouseDownCount);
+}
 
-      rafAbortable(mouseUpSignal, () => {
-        const nowDiff = performance.now() - mouseUpCurrent;
-        const progress = nowDiff / animDurationMs;
-        let count = Math.floor(progress * frames) + (frames - mouseDownCount);
-        if (count > frames) {
-          mouseUpController.abort();
-          return;
-        }
-        if (count < 0) {
-          count = 0;
-        }
-        const frameNum = prefixNumberWithZeros(
-          frames - count,
-          2
-        );
-    
-        bowties.style.backgroundImage = `url("/bowtie-button-demo/assets/frame${frameNum}.svg")`;
-      });
-    },
-    { once: true }
-  );
+function stopListening(mouseDownController, mouseDownCount) {
+  mouseDownController.abort();
+  const mouseUpController = new AbortController();
+  const mouseUpSignal = mouseUpController.signal;
+  const mouseUpCurrent = performance.now();
+
+  rafAbortable(mouseUpSignal, () => {
+    const nowDiff = performance.now() - mouseUpCurrent;
+    const progress = nowDiff / animDurationMs;
+    let count = Math.floor(progress * frames) + (frames - mouseDownCount.current);
+    if (count > frames) {
+      mouseUpController.abort();
+      return;
+    }
+    if (count < 0) {
+      count = 0;
+    }
+    const frameNum = prefixNumberWithZeros(
+      frames - count,
+      2
+    );
+
+    bowties.style.backgroundImage = `url("/bowtie-button-demo/assets/frame${frameNum}.svg")`;
+  });
+}
+
+const el = document.querySelector('.bowtie-button');
+const bowties = el.querySelector('.bowties');
+
+el.addEventListener('pointerdown', () => {
+  startListening((mouseDownController, mouseDownCount) => {
+    document.addEventListener(
+      'pointerup',
+      () => {
+        stopListening(mouseDownController, mouseDownCount);
+      },
+      { once: true }
+    );
+  })
 });
+
+let timeout = undefined;
+const keydownTrottle = throttle(event => {
+  if (event.key !== 'Enter' && event.key !== " ") return;
+  startListening((mouseUpController) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      stopListening(mouseUpController, {current: 15});
+    }, animDurationMs)
+  })
+}, 500);
+
+el.addEventListener('keydown', keydownTrottle)
